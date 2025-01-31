a2b48382e9e462b0b8f0741a96f28cf72fa930b3
maykon3 <mkn.silva29@gmail.com>
2025-01-31 16:33:18 -0300

    first commit maykon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.movi_mais"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.movi_mais"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
