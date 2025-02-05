d33e820c85a73eccb9fd57ff7bc9b2f46d0a01c5
maykon3 <mkn.silva29@gmail.com>
2025-02-05 12:34:09 -0300

    style: Criação de tela splash

--- app/src/main/java/com/example/movi_mais/MainActivity.kt
+++ app/src/main/java/com/example/movi_mais/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,8 +15,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.movi_mais.ui.theme.CorBege
 import com.example.movi_mais.ui.theme.Movi_MaisTheme
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,24 +43,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Inicio() {
-  Column(
-      verticalArrangement = Arrangement.Center,
-      horizontalAlignment = Alignment.CenterHorizontally,
-      modifier = Modifier
-<<<<<<< HEAD
-          .fillMaxSize()
-  ) {
-      
-  }
-=======
-  ) {  }
->>>>>>> 2a7db65ab4ce6c2d2d4907b0d4928b9c4e26c395
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .paint(
+                painterResource(
+                    id = R.drawable.backgroud
+                ),
+                contentScale = ContentScale.Crop
+            )
+            .fillMaxSize()
+
+    ) {
+
+        Spacer(Modifier.padding(top = 64.dp))
+
+        Text(
+            "Somos Todos",
+            fontWeight = FontWeight.Bold,
+            fontSize = 48.sp,
+            color = CorBege,
+            style = androidx.compose.ui.text.TextStyle(
+                shadow = Shadow(
+                    color = CorBege,
+                    offset = Offset(0f, 10f),
+                    blurRadius = 20f
+                )
+            )
+
+
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Movi_MaisTheme {
-
+        Inicio()
     }
 }
